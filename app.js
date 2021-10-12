63dff64f3b4b6922d01774e4f9ded2236e2de4a3
Donovan Gallaway <donovangallaway@gmail.com>
2021-10-12 10:37:43 -0700

    updated readme and added some more mobile responsiveness

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 // Contentful site hosting the trivia qs
 const URL = "https://cdn.contentful.com/spaces/5o940t3dtq1j/environments/master/entries?access_token=-0DOA4rCTbI7F2rOP4wjBuJveqEqrU9mHULCJ3tUg8M&content_type=triviaQuestion"
 
+
+// Setting the score as a global variable so it can be acessed by any needed functions
 let p1Score = 0;
 let p2Score = 0;
 let p1Turn = true;
 
+// Score add function to save a bit of clarity
 const scoreAdd = (player) => {
     if (player === 1) {
         p1Score++
@@ -19,22 +22,18 @@ const scoreAdd = (player) => {
 }
 
 
+// Set up the game. I did this in a function instead of a variable because I intended to add a title screen as a separate function, but due to the nature of the promise call I may end up just making this a global thing and putting the title screen as a function inside this whole "Main Game"
 
-const mainGame = () => {
-    $.ajax(URL).then((data)=> {
-        console.log(data)
-        let i = 0;
-        questionQueue(i, data)
-    })
-}
-
+// This not only makes for slightly drier code, but also gives me an easy way to start the game when I want
+// Note: due to the data being called as avariable inside "main game", this has to be inside "main game", which is another argument for making the promise request globally accessible.
 const questionQueue = (index, data) => {
-    $('#game').empty();
-    $('#score1').text(p1Score)
+    $('#game').empty(); // Reset game state
+    $('#score1').text(p1Score) //Update scores (0 for reset state)
     $('#score2').text(p2Score)
 
+    // Text refers to the question text, while correct refers to the answer text
     const compareAnswers = (text, correct) => {
-        $('#game').empty() 
+        $('#game').empty() //Displays the answer next, so emptying makes it cleaner
         if (text===correct){
              $('#game').append($('<h1>').text("Correct!").css('color','green').attr('id','truth'))
              if (p1Turn) {
@@ -45,12 +44,15 @@ const questionQueue = (index, data) => {
          } else {
              $('#game').append($('<h1>').text("Wrong!").css('color','red').attr('id','truth'))
          }
-         console.log(data.items.length)
-         console.log(index)
+         $('#game').append($('<p>').text('Click above to continue'))
+         $('#game').append($('<h3>').text(data.items[index].fields.answerText))
+        //  console.log(data.items.length)
+        //  console.log(index)
+        // This if statement effectively iterates through the questions as long as a question exists. If it doesn't, we tally up the score
          if (data.items[index+1]){
              $('#truth').on('click', () => {
                 console.log('clicked')
-                questionQueue(index+1, data)
+                questionQueue(index+1, data) 
              })
          }
          else {
@@ -67,10 +69,10 @@ const questionQueue = (index, data) => {
     .append($('<li>').text(data.items[index].fields.c).attr('id','c'))
     .append($('<li>').text(data.items[index].fields.d).attr('id','d'))
     const corrAnswer = data.items[index].fields.answer
-    console.log(corrAnswer)
+    // console.log(corrAnswer)
     $('#game').append($question).append($answers)
 
-    // Add on-click function for answers
+    // Add on-click function for answers. The compareAnswers function also resets game state
     $('#a').on('click', () => {
         compareAnswers($('#a').text(), corrAnswer)
     })
@@ -84,6 +86,7 @@ const questionQueue = (index, data) => {
         compareAnswers($('#d').text(), corrAnswer)
     })
     const gameOver = () => {
+        // Originally had this empty out the whole body and try to reset that way, but that killed the divs I had hard-coded into html, so I added some stuff to empty out so the game could be reset.
         $('#game').empty()
         $('#game').append($('<h1>').text("Well done!"))
         if (p1Score > p2Score) {
@@ -94,9 +97,11 @@ const questionQueue = (index, data) => {
             $('#result').append($('<h2>').text("A Draw!"))
         }
         $('#result').append($('<h4>').text('Click here to play again').addClass('reset'))
+        // Reset the invisible game state
         p1Score = 0;
         p2Score = 0;
         p1Turn = true;
+        // On click, visually reset game state
         $('.reset').on('click', ()=>{
             $('#game').empty()
             $('#result').empty()
@@ -105,27 +110,9 @@ const questionQueue = (index, data) => {
     }
     
 }
-
-
-const gameOver = () => {
-    $('body').empty()
-    $('body').append($('<h1>').text("Well done!"))
-    if (p1Score > p2Score) {
-        $('body').append($('<h2>').text("Player 1 Wins!"))
-    } else if (p1Score < p2Score) {
-        $('body').append($('<h2>').text("Player 2 Wins!"))
-    } else if (p1Score === p2Score) {
-        $('body').append($('<h2>').text("A Draw!"))
-    }
-    $('body').append($('<h4>').text('Click anywhere to play again'))
-    p1Score = 0;
-    p2Score = 0;
-    p1Turn = true;
-    $('body').on('click', ()=>{
-        $('body').empty()
-        questionQueue(0,data)
+// Grabs the trivia data
+$.ajax(URL).then((data)=> {
+    console.log(data)
+    let i = 0; // This i acts as an iterator through the various runs of the function without needing to have everything in a giant "for" loop
+    questionQueue(i, data)
     })
-}
-
-
-mainGame()
--- readme.md
+++ readme.md
@@ -4,7 +4,11 @@
 
 ## Introduction
 
-Explain what is your project here, what it does, etc.
+I decided to go with a trivia game based on the novel *Dune*, in preparation for the upcoming Denis Villeneuve adaptation (in addition to the fact that I'm just a giant Dune nerd).
+
+The strucutre of the app itself was put together mostly in jQuery, which was done specifically to open up the ability to add some animated and responsive pages such as a title screen and a brief introductory text, making it feel more like a JS game than a trivia game HTML page.
+
+In terms of responsiveness, I wanted it to work pretty readily on both mobile and desktop browsers, so took care to use primarily responsive units in my CSS and lay out my game in such a way that was easy to see on both. There is a minimum of text and no in-line images used, so the site scales down very well in terms of size, though it looks a bit sparse on very high resolution desktops.
 
 ## Technologies
 
@@ -13,25 +17,28 @@ Explain what is your project here, what it does, etc.
 - JS
 - JQuery
 
-## Challengies
+## Challenges
 
-#### There was some trouble with
+#### Scoping
 
-My code looked like this
+Due to the fact that I was initially unfamiliar with Ajax, I ran into a problem in laying out my functions, specifically in the scope of where the data was accessible.
 
-```js
-console.log("hello")
-```
-
-I fixed it with 
+However, I solved the problem by passing the data to my main game function, where it could then be referenced by lower-order functions:
 
 ```js
-console.log("fixed")
+$.ajax(URL).then((data)=> {
+    questionQueue(i, data)
+    })
 ```
 
 
-#### Example Table
+#### Areas for Improvement
+
+I still need to find appropriate images, code the animations, and otherwise style the page. If I have enough time after that, I might try and use a game/animation library to add some spice to the animation/game states.
+
+
+<!-- #### Example Table
 
 | Column1 | Column2 | Column3|
 |---------|---------|--------|
-| thing 1 | thing 2 | thing 3|
+| thing 1 | thing 2 | thing 3| -->
